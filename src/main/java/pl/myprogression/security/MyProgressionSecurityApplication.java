4627f00c896a6a4a5c0c013dd4b94532b0c8c3f2
KrystianKorbecki <krystian.korbecki36@interia.pl>
2021-09-24 18:55:13 +0200

    init commit

---
+++ src/main/java/pl/myprogression/security/MyProgressionSecurityApplication.java
@@ -0,0 +1,17 @@
+package pl.myprogression.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MyProgressionSecurityApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MyProgressionSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/myprogression/security/MyProgressionSecurityApplicationTests.java
@@ -0,0 +1,15 @@
+package pl.myprogression.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProgressionSecurityApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
