28e374539a7e3128ea175c6d2b0bfe209f88ccdd
KrystianKorbecki <krystian.korbecki36@interia.pl>
2021-09-25 15:38:19 +0200

    Add liquidbase changeset and mapping entities

--- src/test/java/pl/myprogression/security/MyProgressionSecurityApplicationTests.java
+++ src/test/java/pl/myprogression/security/MyProgressionSecurityApplicationTests.java
@@ -3,6 +3,9 @@ package pl.myprogression.security;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.security.Provider;
+import java.security.Security;
+
 @SpringBootTest
 class MyProgressionSecurityApplicationTests
 {
@@ -10,6 +13,16 @@ class MyProgressionSecurityApplicationTests
     @Test
     void contextLoads()
     {
+    }
+
+    @Test
+    void algorithm() {
+        for (Provider provider : Security.getProviders()) {
+            System.out.println("Provider: " + provider.getName());
+            for (Provider.Service service : provider.getServices()) {
+                System.out.println("  Algorithm: " + service.getAlgorithm());
+            }
+        }
     }
 
 }
